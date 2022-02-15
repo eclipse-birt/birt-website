472e53cdd3fedb97a3a228d4cae2937617b9647c
Christophe Loetz <chloetz@compex-commerce.com>
2022-02-15 17:51:56 +0100

    Repaired missing baseUrl: '/birt-website/'

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ module.exports = {
   title: 'BIRT',
   tagline: 'Business Intelligence Reporting Tool',
   url: 'https://eclipse.github.io/',
-  baseUrl: '/',
+  baseUrl: '/birt-website/',
   onBrokenLinks: 'ignore', // default = warn
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/BIRT-Icon.ico/',
