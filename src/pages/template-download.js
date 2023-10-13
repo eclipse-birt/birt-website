3ff896fa9e3873ed08599003bb37710b5cc86779
TheSoundOfScience <85256613+TheSoundOfScience@users.noreply.github.com>
2023-10-13 13:35:57 +0200

    fix template download URL

--- src/pages/template-download.js
+++ src/pages/template-download.js
@@ -52,7 +52,7 @@ function Hello() {
           <div class="alert alert--primary" role="alert">
             <center>
               <button class="button button--lg button--primary">
-                <a href="/static/templates/BIRT-templates-download_V4.9.zip" download>Click to Download Templates and Examples</a>
+                <a href="/birt-website/templates/BIRT-templates-download_V4.9.zip" download>Click to Download Templates and Examples</a>
               </button> 
             </center>
           </div>
