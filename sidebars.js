f92f134775c9b663443417b496921074566c0d97
Wim Jongman <wim.jongman@remainsoftware.com>
2026-08-21 15:53:59 +0200

    Add 'report-generation' to Documentation section

--- sidebars.js
+++ sidebars.js
@@ -5,7 +5,7 @@ module.exports = {
   someSidebar: {
     About: ['what-is-birt','overview-design','overview-architecture','overview-designer','overview-customization','overview-extensibility', 'contributors','bibliography'],
     "Getting Started": ['installation', 'create-a-report','my-first-report', 'community'],
-    Documentation: ['t_brief-editor-tour','t_grids','t_data-sources','t_data-sets','t_data-properties','t_cascading-styles','t_create-a-report','t_styles','t_tables','t_testing-reports','t_text-item','t_tools','t_visual-properties'],
+    Documentation: ['t_brief-editor-tour','t_grids','t_data-sources','t_data-sets','t_data-properties','t_cascading-styles','t_create-a-report','t_styles','t_tables','t_testing-reports','t_text-item','t_tools','t_visual-properties','report-generation'],
     Integrating: ['integrating/integrating',  'integrating/viewer-setup', 'integrating/viewer-usage', 'integrating/using-php', 'integrating/deapi', 'integrating/reapi'],
     "How to contribute": ['how2contribute'],
     "Templates & Examples": ['template-introduction','template-sample-database'],
